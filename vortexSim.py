23786ff1ee57dfb79fd14a73011f0b2435863f6f
cibinjoseph <cibinjoseph92@gmail.com>
2020-08-09 02:57:32 +0530

    Initial commit

---
+++ vortexSim.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+""" Simulates motion of vortices """
+import vtx
+import numpy as np
+import matplotlib.pyplot as plt
+# import matplotlib.animation as animation
+from mpl_toolkits.mplot3d import axes3d
+from celluloid import Camera
+
+def getCoords(vts):
+    """ Get coordinates of vortices in x, y, z form """
+    coords = [[], [], []]
+    for vt in vts:
+        coords = vt.appCoords(coords)
+    return coords
+
+dt = 0.01
+nt = 50
+
+# Read coordinates of vortices 
+vts = np.loadtxt('vorticesGen.dat')
+shape = np.shape(vts)
+nVortices = shape[0]
+
+# Initialize vortices
+vortx = []
+if shape[1] == 6:
+    for vt in vts:
+        vortx.append(vtx.Vtx([vt[0], vt[1], vt[2]], \
+                             [vt[3], vt[4], vt[5]], \
+                             1.0, 0.0))
+elif shape[1] == 8:
+    for vt in vts:
+        vortx.append(vtx.Vtx([vt[0], vt[1], vt[2]], \
+                             [vt[3], vt[4], vt[5]], \
+                             vt[6], vt[7]))
+else:
+    raise ValueError('Wrong number of columns in vortex coordinate file')
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+camera = Camera(fig)
+for i in range(nt):
+    print(str(i) + '/' + str(nt))
+    coords = getCoords(vortx)
+
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    ax.plot(coords[0],coords[1],coords[2],'b')
+    camera.snap()
+
+    for vt in vortx:
+        for vtOther in vortx:
+            vt.addInfluenceBy(vtOther)
+
+    for vt in vortx:
+        vt.update(dt)
+
+animation = camera.animate()
+animation.save('celluloid.gif', writer='imagemagick')
---
+++ vtx.py
@@ -0,0 +1,67 @@
+""" Class definition for vortex """
+import numpy as np
+
+
+eps = np.finfo(float).eps
+inv4pi = 0.25/np.pi
+
+class Vtx:
+    P1 = np.array([0,0,0])
+    P2 = np.array([0,0,0])
+    gam = 1.0
+    rc = 0.0
+    vel1 = 0.0
+    vel2 = 0.0
+
+    def __init__(self, P1, P2, gam=1.0, rc=0.0):
+        self.P1 = np.array(P1)
+        self.P2 = np.array(P2)
+        self.gam = gam
+        self.vel1 = 0.0
+        self.vel2 = 0.0
+        if rc < eps:
+            self.rc = 1E-6
+        else:
+            self.rc = rc
+
+    def vind(self, P):
+        """ Induced velocity at P """
+        r1 = P-self.P1
+        r2 = P-self.P2
+
+        r1Xr2 = np.cross(r1,r2)
+        r1Xr2Abs2 = np.dot(r1Xr2, r1Xr2)
+
+        vel = np.array([0.0, 0.0, 0.0])
+        if r1Xr2Abs2 > eps:
+            r0 = r1-r2
+            r1Unit = r1/np.linalg.norm(r1)
+            r2Unit = r2/np.linalg.norm(r2)
+            vel = self.gam*r1Xr2*inv4pi*np.dot(r0, r1Unit-r2Unit) / \
+                    np.sqrt((self.rc*np.linalg.norm(r0))**4.0 + r1Xr2Abs2**2.0)
+        return vel
+
+    def move(self, vec1, vec2):
+        """ Move the vortex points by vec1 and vec2 """
+        self.P1 = self.P1 + vec1
+        self.P2 = self.P2 + vec2
+
+    def addInfluenceBy(self, vtx2):
+        vel1, vel2 = vtx2.vind(self.P1), vtx2.vind(self.P2)
+        self.vel1 = self.vel1 + vel1
+        self.vel2 = self.vel2 + vel1
+
+    def update(self, dt):
+        self.move(self.vel1*dt, self.vel2*dt)
+        self.vel1 = 0.0
+        self.vel2 = 0.0
+
+    def appCoords(self, coords):
+        if coords is not [[],[],[]]:
+            coords[0].append(np.nan)
+            coords[1].append(np.nan)
+            coords[2].append(np.nan)
+        coords[0].extend([self.P1[0], self.P2[0]])
+        coords[1].extend([self.P1[1], self.P2[1]])
+        coords[2].extend([self.P1[2], self.P2[2]])
+        return coords
