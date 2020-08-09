f6df3d261fa1f99408c9ec194f118d2bc6df4d2e
cibinjoseph <cibinjoseph92@gmail.com>
2020-08-09 18:50:37 +0530

    Add line colour

--- tools/visitPlot.py
+++ tools/visitPlot.py
@@ -1,7 +1,16 @@
 # Run this using visit -s
 OpenDatabase("Results/vortices*.plt database")
 AddPlot("Mesh", "mesh", 1, 1)
-aatts = AnnotationAttributes()
-aatts.axes3D.visible = 1
-SetAnnotationAttributes(aatts)
+
+# Line colour
+MeshAtts = MeshAttributes()
+MeshAtts.lineWidth = 4
+MeshAtts.meshColor = (51, 102, 255, 255)
+MeshAtts.meshColorSource = MeshAtts.MeshCustom
+SetPlotOptions(MeshAtts)
+
+# 3D Axis
+# aatts = AnnotationAttributes()
+# aatts.axes3D.visible = 1
+# SetAnnotationAttributes(aatts)
 DrawPlots()
