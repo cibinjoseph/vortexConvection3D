8f57d38c64f8c55729dc4619316ad28cc0912527
cibinjoseph <cibinjoseph92@gmail.com>
2020-08-09 15:28:31 +0530

    Rename simulation script

--- visitPlot.py
+++ visitPlot.py
@@ -1,4 +1,7 @@
 # Run this using visit -s
 OpenDatabase("Results/vortices*.plt database")
 AddPlot("Mesh", "mesh", 1, 1)
+aatts = AnnotationAttributes()
+aatts.axes3D.visible = 1
+SetAnnotationAttributes(aatts)
 DrawPlots()
