ad394a08b370cb6dbf5ef962ca6aa0449c0b3de6
cibinjoseph <cibinjoseph92@gmail.com>
2020-08-09 12:23:33 +0530

    Add plot script for visit

---
+++ visitPlot.py
@@ -0,0 +1,4 @@
+# Run this using visit -s
+OpenDatabase("Results/vortices*.plt database")
+AddPlot("Mesh", "mesh", 1, 1)
+DrawPlots()
--- vortexSim.py
+++ vortexSim.py
@@ -8,11 +8,17 @@ from mpl_toolkits.mplot3d import axes3d
 from celluloid import Camera
 
 
+# Simulation parameters
+dt = 0.01
+nt = 10
+generateAnimation = False
+generatePlots = True
+
+# Default filenames
 vtxFilamentsFilename = 'vortexFilaments.dat'
 animationFilename = 'output.gif'
 resultsDir = 'Results/'
-dt = 0.01
-nt = 10
+
 
 def getCoords(vts):
     """ Get coordinates of vortices in x, y, z form """
@@ -33,47 +39,57 @@ def write2file(filename, vts):
             fh.write(xyz1 + '\n')
             fh.write(xyz2 + '\n')
 
-# Read coordinates of vortices 
-vts = np.loadtxt(vtxFilamentsFilename)
-shape = np.shape(vts)
-nVortices = shape[0]
-print('Total vortices found: ' + str(nVortices))
+def main():
+    # Read coordinates of vortices 
+    vts = np.loadtxt(vtxFilamentsFilename)
+    shape = np.shape(vts)
+    nVortices = shape[0]
+    print('Total vortices found: ' + str(nVortices))
 
-# Initialize vortices
-vortx = []
-if shape[1] == 6:
-    for vt in vts:
-        vortx.append(vtx.Vtx([vt[0], vt[1], vt[2]], \
-                             [vt[3], vt[4], vt[5]], \
-                             1.0, 0.0))
-elif shape[1] == 8:
-    for vt in vts:
-        vortx.append(vtx.Vtx([vt[0], vt[1], vt[2]], \
-                             [vt[3], vt[4], vt[5]], \
-                             vt[6], vt[7]))
-else:
-    raise ValueError('Wrong number of columns in vortex coordinate file')
+    # Initialize vortices
+    vortx = []
+    if shape[1] == 6:
+        for vt in vts:
+            vortx.append(vtx.Vtx([vt[0], vt[1], vt[2]], \
+                                 [vt[3], vt[4], vt[5]], \
+                                 1.0, 0.0))
+    elif shape[1] == 8:
+        for vt in vts:
+            vortx.append(vtx.Vtx([vt[0], vt[1], vt[2]], \
+                                 [vt[3], vt[4], vt[5]], \
+                                 vt[6], vt[7]))
+    else:
+        raise ValueError('Wrong number of columns in vortex coordinate file')
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-camera = Camera(fig)
-for i in range(nt):
-    timestamp = format(i+1, '05')
-    print(timestamp + '/' + str(nt))
-    coords = getCoords(vortx)
+    if generateAnimation:
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+        camera = Camera(fig)
 
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    ax.plot(coords[0],coords[1],coords[2],'b')
-    camera.snap()
-    write2file(resultsDir + 'vortices' + timestamp + '.plt', vortx)
+    for i in range(nt):
+        timestamp = format(i+1, '05')
+        print(timestamp + '/' + str(nt))
 
-    for vt in vortx:
-        for vtOther in vortx:
-            vt.addInfluenceBy(vtOther)
+        if generateAnimation:
+            coords = getCoords(vortx)
+            plt.xlabel('X-axis')
+            plt.ylabel('Y-axis')
+            ax.plot(coords[0],coords[1],coords[2],'b')
+            camera.snap()
 
-    for vt in vortx:
-        vt.update(dt)
+        if generatePlots:
+            write2file(resultsDir + 'vortices' + timestamp + '.plt', vortx)
 
-animation = camera.animate()
-animation.save('output.gif', writer='imagemagick')
+        for vt in vortx:
+            for vtOther in vortx:
+                vt.addInfluenceBy(vtOther)
+
+        for vt in vortx:
+            vt.update(dt)
+
+    if generateAnimation:
+        animation = camera.animate()
+        animation.save('output.gif', writer='imagemagick')
+
+if __name__ == '__main__':
+    main()
