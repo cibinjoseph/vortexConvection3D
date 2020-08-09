0e9026f7b93ec6a49c512f09fc44d523b71780e9
cibinjoseph <cibinjoseph92@gmail.com>
2020-08-09 18:50:26 +0530

    Add rotation and coordinate shift

--- tools/createRingVortex.py
+++ tools/createRingVortex.py
@@ -4,12 +4,28 @@
 import numpy as np
 import io
 
+nFilaments = 60
 radius = 1.0
-zCoordinate = 0.5
+zCoordinate = 0.0
 xShift = 0.0
-yShift = 0.0
-nFilaments = 25
+yShift = 1.0
+thetaX = 0.0  # Rotation about X-axis after shift
+thetaY = 20.0  # Rotation about Y-axis after shift
 outputString = io.StringIO()
+
+rotX = np.array([[1, 0, 0], \
+                 [0,np.cos(np.deg2rad(thetaX)),-np.sin(np.deg2rad(thetaX))], \
+                 [0,np.sin(np.deg2rad(thetaX)), np.cos(np.deg2rad(thetaX))]])
+rotY = np.array([[np.cos(np.deg2rad(thetaY)),0,-np.sin(np.deg2rad(thetaY))], \
+                 [0,1,0], \
+                 [np.sin(np.deg2rad(thetaY)),0, np.cos(np.deg2rad(thetaY))]])
+
+def rotate(coords):
+    rotMat = rotY.dot(rotX)
+    rows = np.shape(coords)
+    for i in range(rows[0]):
+        coords[i] = rotMat.dot(coords[i])
+    return coords
 
 # Create coordinates of a circle
 thetas = np.linspace(0.0, 2.0*np.pi, nFilaments+1)
@@ -20,9 +36,11 @@ for theta in thetas:
            zCoordinate]
     vRingStart.append(xyz)
 
+vRingStart = np.array(vRingStart)
+vRingStart = rotate(vRingStart)
+
 # Create start and end points for 
 # each filament in the vortex ring
-vRingStart = np.array(vRingStart)
 vRingEnd = vRingStart[1:, :]
 vRingStart = vRingStart[0:-1, :]
 
